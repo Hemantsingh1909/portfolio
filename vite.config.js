b676cc59c76d3bbbbb86040ac92e6a684b9e382f
Hemant Satwal <hemantsingh1909@gmail.com>
2025-10-05 03:32:58 +0530

    production error 2

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(
-    {babel: {
-        plugins: ['@babel/plugin-transform-private-property-in-object']
-      }}
-  )],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["@babel/plugin-transform-private-property-in-object"],
+      },
+    }),
+  ],
   server: {
+    port: 5173,
     proxy: {
-      "/api": "http://localhost:3000", // proxy API to vercel dev
-    },
-    fs: {
-      strict: false, // Allow access to public files
+      // proxy API to vercel dev (http://localhost:3000)
+      "/api": "http://localhost:3000",
+      fs: {
+        strict: false, // Allow access to public files
+      },
     },
   },
   css: {
     postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
   build: {
-    outDir: 'dist'
+    outDir: "dist",
   },
-})
+});
